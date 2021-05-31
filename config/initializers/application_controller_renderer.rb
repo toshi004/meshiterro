b93967c1ecd940509e55c2b9b8cd5750633216cb
toshi004 <d.toshi004.35@gmail.com>
2021-05-31 16:20:34 +0000

    [Start]4-2

---
+++ config/initializers/application_controller_renderer.rb
@@ -0,0 +1,10 @@
+# Be sure to restart your server when you modify this file.
+
+# ActiveSupport::Reloader.to_prepare do
+#   ApplicationController.renderer.defaults.merge!(
+#     http_host: 'example.org',
+#     https: false
+#   )
+# end
+
+Refile.secret_key = '264fedab2345a170e088f212ba84a1cdf676d5ac22f5d8a7f732e0ccb0ec286f6121bc9282fbd18d31e231c2e6850d9d7666fe39ee2dcea4fe860e37c978b3bf'
